8c49970606382634877d8dd6f25c74152c87c2e8
mercurial1255 <mercurial1255@gmail.com>
2024-07-31 11:42:30 +0500

    todo

--- root/settings.py
+++ root/settings.py
@@ -82,24 +82,24 @@ AUTH_USER_MODEL = 'apps.User'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-#
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": os.getenv('PG_NAME'),
-#         "USER": os.getenv('PG_USER'),
-#         "PASSWORD": os.getenv('PG_PASSWORD'),
-#         "HOST": os.getenv('PG_HOST', 'localhost'),
-#         "PORT": os.getenv('PG_PORT')
-#     }
-# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv('PG_NAME'),
+        "USER": os.getenv('PG_USER'),
+        "PASSWORD": os.getenv('PG_PASSWORD'),
+        "HOST": os.getenv('PG_HOST', 'localhost'),
+        "PORT": os.getenv('PG_PORT')
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
